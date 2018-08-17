0de1ebe80fa5122009ea0975dd374d0171fce26b
Cadox8 <cadox8@gmail.com>
2018-08-17 19:40:34 +0200

    Commons: Updated Card
    Signed-off-by: Cadox8 <cadox8@gmail.com>

--- Commons/src/me/cadox8/commons/Card.java
+++ Commons/src/me/cadox8/commons/Card.java
@@ -3,6 +3,7 @@ package me.cadox8.commons;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 @RequiredArgsConstructor
@@ -13,8 +14,34 @@ public class Card {
     @Setter private BufferedImage base;
     @Setter private int requeiredHoles = -1;
 
-
     public BufferedImage build() {
-        return null;
+        Graphics g = base.getGraphics();
+
+        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+
+        if (text.split(" ").length <= 3) {
+            g.drawString(text, 20, 30);
+        } else {
+            int x = 0;
+            for (String s : formatText()) {
+                g.drawString(s, 20, 30 + (x * g.getFont().getSize()));
+                x++;
+            }
+        }
+        g.dispose();
+        return base;
+    }
+
+    private String[] formatText() {
+        String[] parts = text.split(" ");
+        final String[] text = {"", "", "", ""};
+
+        int path = 0;
+        for (int x = 0; x < parts.length; x++) {
+            text[path] += parts[x] + " ";
+
+            if (x != 0 && x % 3 == 0) path++;
+        }
+        return text;
     }
 }
